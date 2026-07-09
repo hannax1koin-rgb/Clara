a600905862d936b3e2b9f9198b19fb5fdd482fe2
hannax1koin-rgb <hannax1.koin@gmail.com>
2026-07-10 01:52:07 +0300

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./db/schema.ts",
+  schema: "./db/schema.js",
   out: "netlify/database/migrations",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
 });
